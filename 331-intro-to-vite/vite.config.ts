663a3773b946ebc9d19014026b4fe505e632cfae
DeformiaR <113496368+DeformiaR@users.noreply.github.com>
2024-08-10 03:37:51 +0700

    set1

--- 331-intro-to-vite/vite.config.ts
+++ 331-intro-to-vite/vite.config.ts
@@ -1,18 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+  plugins: [react()],
+  build:{
+    outDir: "build"
   }
 })
